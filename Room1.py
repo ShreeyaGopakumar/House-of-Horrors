eb335a3f26be5657c59fd255cd3036ac2b47bbf0
ShreeyaGopakumar <sgopakum@andrew.cmu.edu>
2023-11-24 00:54:51 -0500

    12.54 24/11

--- Room1.py
+++ Room1.py
@@ -3,5 +3,5 @@ from cmu_graphics import *
 def redrawAll(app):
     drawRect(0,0,app.width,app.height,fill="maroon")
     drawImage(app.drawer1,0,app.height//2-150,width=app.drawer1Width,height=app.drawer1Height)
-
+    
 
